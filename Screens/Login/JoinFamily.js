180fb3881657b0df78b73f5aeb32c0477c2d901b
Alex Pun <punalex01@gmail.com>
2022-03-08 23:51:29 -0800

    Finished JoinFamily styling

--- Screens/Login/JoinFamily.js
+++ Screens/Login/JoinFamily.js
@@ -15,33 +15,54 @@ const styles = StyleSheet.create({
     width: '100%'
   },
   cardStyle: {
+    position: 'absolute',
     display: 'flex',
     backgroundColor: '#F5F1E9',
-    height: '50%',
-    width: '80%',
-    alignItems: 'center',
+    width: 347,
+    height: 470,
+    borderRadius: 10,
+    alignSelf: 'center',
     justifyContent: 'center',
-    paddingTop: 100
+  },
+  cardContainer: {
+    alignSelf: 'center',
+    alignItems: 'center',
+    width: '100%',
+    margin: 'auto'
   },
   text: {
     fontSize: 32,
-    color: 'white',
+    color: '#4C5A9E',
     marginBottom: 20,
     textAlign: 'center',
-    top: '20%'
+    top: '20%',
+    width: '100%'
   },
   inputs: {
-    width: '80%',
+    width: 282,
+    height: 59,
     borderRadius: 10,
-    marginBottom: 32
+    marginBottom: 20,
+    backgroundColor: 'white',
   },
   button: {
     width: '30%',
-    backgroundColor: 'black',
-    borderRadius: 10,
+    backgroundColor: '#4C5A9E',
+    borderRadius: 5,
+    width: 124,
+    height: 37
   },
   buttonText: {
     color: 'white'
+  },
+  forgotContainer: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  forgotText: {
+    color: '#4C5A9E',
+    fontSize: 14,
+    lineHeight: 20
   }
 });
 
@@ -51,34 +72,42 @@ export const JoinFamily = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
       <Card style={styles.cardStyle}>
-        <Text style={styles.text}>Family ID</Text>
-        <TextInput
-          style={styles.inputs}
-          placeholder="Family ID"
-          value={familyId}
-          onChangeText={familyId => setFamilyId(familyId)}
-        />
-        <Button
-          style={styles.button}
-          labelStyle={styles.buttonText}
-          onPress={() => {
-            if (navigation.getParam('isParent')){
-              const userRef = doc(db, "Parents", auth.currentUser.uid)
-              updateDoc(userRef, {
-                familyId: familyId,
-              })
-            }
-            else
-            {
-              const userRef = doc(db, "Kids", auth.currentUser.uid)
-              updateDoc(userRef, {
-                familyId: familyId,
-              })
-            }
-            joinFamily(familyId, auth.currentUser.uid, navigation.getParam('isParent'))
-          }}>
-            Join
-        </Button>
+        <View style={styles.cardContainer}>
+          <Text style={styles.text}>Family ID</Text>
+          <TextInput
+            style={styles.inputs}
+            placeholder="Family ID"
+            value={familyId}
+            onChangeText={familyId => setFamilyId(familyId)}
+          />
+          <Button
+            style={styles.button}
+            labelStyle={styles.buttonText}
+            onPress={() => {
+              if (navigation.getParam('isParent')){
+                const userRef = doc(db, "Parents", auth.currentUser.uid)
+                updateDoc(userRef, {
+                  familyId: familyId,
+                })
+              }
+              else
+              {
+                const userRef = doc(db, "Kids", auth.currentUser.uid)
+                updateDoc(userRef, {
+                  familyId: familyId,
+                })
+              }
+              joinFamily(familyId, auth.currentUser.uid, navigation.getParam('isParent'))
+            }}>
+              Join
+          </Button>
+          <View style={styles.forgotContainer}>
+            <Button style={styles.signup} labelStyle={styles.forgotText} uppercase={false}>Parent Sign Up</Button>
+            <Button labelStyle={styles.forgotText} uppercase={false}>Forgot family ID</Button>
+          </View>
+          
+        </View>
+        
       </Card>
     </SafeAreaView>
   );
